4ab103bef151ec963caefcb7cb348a8ff4c9b4dc
schqvho <alvision07@gmail.com>
2025-12-22 01:25:54 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -144,13 +144,13 @@ if ! $MONGO_RUNNING; then
     apt-get update
     apt-get install -y gnupg curl
 
-    curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
-       gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg --dearmor
+    curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | \
+       gpg -o /usr/share/keyrings/mongodb-server-4.4.gpg --dearmor
 
     UBUNTU_VERSION=$(lsb_release -cs)
     case "$UBUNTU_VERSION" in
       noble|jammy|focal)
-        MONGO_REPO="deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu $UBUNTU_VERSION/mongodb-org/8.0 multiverse"
+        MONGO_REPO="deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-4.4.gpg ] https://repo.mongodb.org/apt/ubuntu $UBUNTU_VERSION/mongodb-org/4.4 multiverse"
         ;;
       *)
         echo -e "${RED}Unsupported Ubuntu version: $UBUNTU_VERSION${NC}"
